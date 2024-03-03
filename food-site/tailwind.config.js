95e48c02aaa7d35ce2615e18dcdf590dee278f0a
hellraisernav <nowaratall@gmail.com>
2024-03-03 13:47:03 +0330

    Build the New css

--- food-site/tailwind.config.js
+++ food-site/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: {
-                "cta-bg": "url('../src/bgmarble.webp')"
+                "cta-bg": "url('bgmarble.webp')"
             }
         }
     },
